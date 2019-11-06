56dfd738fd3eb5cc786f41583ed439f572e60164
Douglas <douglaswelcome@gmail.com>
2019-11-06 13:53:55 -0800

    get that image ftp kickin

--- replace.js
+++
@@ -1,7 +0,0 @@
-//Load the library and specify options
-const replace = require('replace-in-file');
-const options = {
-  files: '_codeDist/markup/templates_email/**/*.html',
-  from: '<!doctype html>',
-  to: '<!--templateType: email isAvailableForNewContent: true--> <!doctype html>' ,
-};
