709cca45496e102d8d462a6321ba6c0f5509439a
Douglas <douglaswelcome@gmail.com>
2020-03-23 13:12:21 -0700

    get that rich text module rolling

--- email_process.sh
+++ email_process.sh
@@ -1,10 +1,10 @@
-for f in _src/markup/email_modules/components/*.mjml; do
-  file="${f##*/}"
-  file="${file%.*}"
-  npx hs create module $file _codeDist/markup/email/email_modules
-  npx mjml _src/markup/email_modules/components/$file.mjml -o _codeDist/markup/email/email_modules/$file.module/module.html
-  echo "gotcha"
-done
+# for f in _src/markup/email_modules/components/*.mjml; do
+#   file="${f##*/}"
+#   file="${file%.*}"
+#   npx hs create module $file _codeDist/markup/email/email_modules
+#   npx mjml _src/markup/email_modules/components/$file.mjml -o _codeDist/markup/email/email_modules/$file.module/module.html
+#   echo "gotcha"
+# done
 
 # for x in _src/markup/email_modules/static/*.mjml; do
 #   static="${x##*/}"
@@ -12,3 +12,5 @@ done
 #   npx mjml _src/markup/email_modules/static/$static.mjml -o _codeDist/markup/email/email_static/$static.html
 #   echo "blam"
 # done
+
+npx mjml _src/markup/email_modules/components/rich-text.mjml -o _codeDist/markup/email/email_modules/module.html
