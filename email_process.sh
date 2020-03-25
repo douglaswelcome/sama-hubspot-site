8ebafc8e2b4c6ffe957f613bec5a1f4753867e52
Douglas <douglaswelcome@gmail.com>
2020-03-24 21:00:08 -0700

    cleaning this thing up

--- email_process.sh
+++ email_process.sh
@@ -13,19 +13,19 @@ for f in _src/markup/email_modules/components/*.mjml; do
   echo "$file module markup converted"
 done
 
-# for x in _src/markup/email_modules/static/*.mjml; do
-#   static="${x##*/}"
-#   static="${static%.*}"
-#   npx mjml _src/markup/email_modules/static/$static.mjml -o _codeDist/markup/email/email_static/$static.html
-#   if grep -q '<!-- begin module -->' _codeDist/markup/email/email_static/$static.html; then
-#    sed -n '/<!-- begin module -->/,/<!-- end module -->/ p ' _codeDist/markup/email/email_static/$static.html > _codeDist/markup/email/email_static/$static-temp.html
-#    mv _codeDist/markup/email/email_static/$static-temp.html _codeDist/markup/email/email_static/$static.html
-#    echo "$static block created"
-#   else
-#     echo "$static block created"
-#   fi
+for x in _src/markup/email_modules/static/*.mjml; do
+  static="${x##*/}"
+  static="${static%.*}"
+  npx mjml _src/markup/email_modules/static/$static.mjml -o _codeDist/markup/email/email_static/$static.html
+  if grep -q '<!-- begin module -->' _codeDist/markup/email/email_static/$static.html; then
+   sed -n '/<!-- begin module -->/,/<!-- end module -->/ p ' _codeDist/markup/email/email_static/$static.html > _codeDist/markup/email/email_static/$static-temp.html
+   mv _codeDist/markup/email/email_static/$static-temp.html _codeDist/markup/email/email_static/$static.html
+   echo "$static block created"
+  else
+    echo "$static block created"
+  fi
   
-# done
+done
 
 # npx mjml _src/markup/email_modules/static/divider.mjml -o _codeDist/markup/email/email_static/divider.html
 
