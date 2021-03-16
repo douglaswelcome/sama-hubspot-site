851636049e77f65378a0b342cdf953907d04dd13
Douglas <douglaswelcome@gmail.com>
2021-03-16 08:29:57 -0700

    Revert "Merge remote-tracking branch 'samasource_site_BOOM/featured/fix-lottie-start' into develop"
    This reverts commit ffff229423a88fed2b296d82435614916c655d03.

--- _src/js/compile/sama/feature-window.js
+++ _src/js/compile/sama/feature-window.js
@@ -1,77 +1,22 @@
-function featureWindow(){
-    const featureSelectors = document.querySelectorAll('.feature-window__selector');
-    const featureDetails = document.querySelectorAll('.feature-window__detail-wrapper');
-    const lottieAnimation = (lottie.getRegisteredAnimations().length > 0 ? true : false);
-    let activeFeature = 0;
+function featureWindow() {
 
-    const handleFeatureChange = function(i) {
+    const featureSelector = document.querySelectorAll('.feature-window__selector');
+    const windowContent = document.querySelectorAll('.feature-window__detail-wrapper');
+    
+    // add the active to 
+    $('.feature-window__selector').click(function () {
+        var i = $(this).index()
+
+        $('.feature-window__selector').removeClass('active');
+        featureSelector[i].classList.add('active');
+
         
-        featureSelectors[activeFeature].classList.remove('active');
-        featureDetails[activeFeature].classList.remove('active');
-
-        featureSelectors[i].classList.add('active');
-        featureDetails[i].classList.add('active');
-
-        if(lottieAnimation){
-            lottie.getRegisteredAnimations()[i].play();
-            lottie.getRegisteredAnimations()[activeFeature].pause();
-        }
-
-        activeFeature = i;
-    }
-
-    featureSelectors.forEach(function (featureSelector, i) {
-
-        if(featureSelector.classList.contains('active') && activeFeature !== i){
-            activeFeature = i;
-        }
-
-        featureSelector.addEventListener('click', function () {
-            handleFeatureChange(i);
-        });
+        $('.feature-window__detail-wrapper').removeClass('active');
+        windowContent[i].classList.add('active');
     });
-
-    //handle lottie loading
-    if(lottieAnimation){
-        function firstAnimationPlay(){
-            const moduleTop = document.querySelector('.feature-window__detail-wrapper.active .feature-window__img');
-
-            function isElementInViewport(el) {
-                const rect = el.getBoundingClientRect();
-                //return true if element is 50% visible
-                return (
-                    rect.top + (rect.height/2) > 0 && // top
-                    rect.left + (rect.width/2) > 0 && // left
-                    rect.top + (rect.height/2) < (window.innerHeight || document.documentElement.clientHeight) && // bottom
-                    rect.left + (rect.width/2) < (window.innerWidth || document.documentElement.clientWidth) // right
-                );
-            }
-
-            function onVisibilityChange(el, callback) {
-                return function () {
-                    const visible = isElementInViewport(el);
-                    if (visible) {
-                        callback();
-                    }
-                }
-            }
-            
-            const  handleAnimationStart = onVisibilityChange(moduleTop, function() {
-                lottie.getRegisteredAnimations()[0].play();
-                window.removeEventListener('scroll', handleAnimationStart, false);
-            });
-            
-            window.addEventListener('DOMContentLoaded', handleAnimationStart, false);
-            window.addEventListener('scroll', handleAnimationStart, false);
-        }
-        
-        firstAnimationPlay();
-    }
 }
 
-featureWindow();
-
-
+$(featureWindow());
 
 
 
