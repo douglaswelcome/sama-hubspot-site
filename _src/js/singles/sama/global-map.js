224b20ef455337da455057b79973b585d0f05e99
Amy Simpson <amysimpson@Sam210110.local>
2021-07-28 09:31:51 -0700

    finishing up modules

---
+++ _src/js/singles/sama/global-map.js
@@ -0,0 +1,119 @@
+buildOfficeList(offices);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWVzaW1wc29uIiwiYSI6ImNrcm1vdGM3eDd2ZTMycHBheTEwajhqcjMifQ.nHuWTNRfeQ83cdMK2qJnpw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/aesimpson/ckrmovdwb2j2r17lqxf2p2lw5',
+    center: [-33.654541, 35.323946],
+    zoom: 1
+});
+
+map.addControl(new mapboxgl.NavigationControl({
+    showCompass: false,
+}));
+
+map.on('load', function (e) {
+    map.addSource('places', {
+        type: 'geojson',
+        data: offices
+    });
+});
+
+offices.features.forEach(function (marker, i) {
+    // Create a div element for the marker
+    var el = document.createElement('div');
+    // Add a class called 'marker' to each div
+    el.className = 'bodymove_map_marker';
+    el.id = "marker-" + i;
+
+
+    if(marker.properties.city){
+        const labelClass = "bodymove_map_marker_label " + (marker.properties.labelPosition ? marker.properties.labelPosition : "");
+        el.innerHTML  = "<p class='"+labelClass+"'>"+marker.properties.city+"</p>";
+    }
+
+    // By default the image for your custom marker will be anchored
+    // by its center. Adjust the position accordingly
+    // Create the custom markers, set their position, and add to map
+    new mapboxgl.Marker(el, {
+            
+        })
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+
+    el.addEventListener('mouseenter', function (e) {
+        createPopUp(marker);
+    });
+});
+
+function flyToDefault() {
+    map.flyTo({
+        center: [-24.547817, 36.980944],
+        zoom: 1
+    });
+    var poppers = document.getElementsByClassName('mapboxgl-popup');
+    // Check if there is already a popup on the map and if so, remove it
+    if (poppers[0]) poppers[0].remove();
+}
+
+function createPopUp(currentFeature) {
+    removePopup();
+
+    var popup = new mapboxgl.Popup({
+            closeOnClick: true
+        })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML('<p>' + currentFeature.properties.title + '</p>' + '<p>' + currentFeature.properties.FTE + ' Full Time Employees</p>')
+        .addTo(map);
+}
+
+function removePopup() {
+    var popUps = document.getElementsByClassName('mapboxgl-popup');
+    // Check if there is already a popup on the map and if so, remove it
+    if (popUps[0]) popUps[0].remove();
+};
+
+// This will let you use the .remove() function later on
+if (!('remove' in Element.prototype)) {
+    Element.prototype.remove = function () {
+        if (this.parentNode) {
+            this.parentNode.removeChild(this);
+        }
+    };
+}
+
+function buildOfficeList(data) {
+    // Iterate through the list of stores
+    for (i = 0; i < data.features.length; i++) {
+        var currentFeature = data.features[i];
+        var prop = currentFeature.properties;
+    }
+}
+
+// Add an event listeners for each marked point on the map
+map.on('click', function (e) {
+    // Query all the rendered points in the view
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ['locations']
+    });
+    if (features.length) {
+        var clickedPoint = features[0];
+        createPopUp(clickedPoint);
+        // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+        var selectedFeature = clickedPoint.properties.address;
+
+        for (var i = 0; i < stores.features.length; i++) {
+            if (stores.features[i].properties.address === selectedFeature) {
+                selectedFeatureIndex = i;
+            }
+        }
+    }
+});
+
+// glober dudes. zoom it out
+window.onload = function () {
+    var globers = document.getElementById('globe');
+    globers.onclick = function () {
+        flyToDefault();
+    };
+};
