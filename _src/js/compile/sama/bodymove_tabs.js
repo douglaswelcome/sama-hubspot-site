c3dccb0af61f726174067a9d3a30edfa04bf5184
Amy Simpson <amysimpson@Sam210110.local>
2021-10-07 17:00:04 -0700

    adding sliding animation

--- _src/js/compile/sama/bodymove_tabs.js
+++ _src/js/compile/sama/bodymove_tabs.js
@@ -1,9 +1,10 @@
 if(document.querySelector('.bodymove_tabs')){
-    const items = document.querySelectorAll('.bodymove_tabs_tab');
 
     const setOrder = function(active){
         const current = document.querySelector('.bodymove_tabs_nav_item.active');
         const reverse = (parseInt(current.getAttribute('data-at')) > active ? true : false);
+         
+        //underline direction
         if(reverse){
             document.querySelector('.bodymove_tabs_navInner').classList.add('slide-back');
         }else{
@@ -13,8 +14,7 @@ if(document.querySelector('.bodymove_tabs')){
         current.classList.remove('active');
         document.querySelectorAll('.bodymove_tabs_nav_item')[active].classList.add('active');
 
-        document.querySelector('.bodymove_tabs_tab.active').classList.remove('active');
-        items[active].classList.add('active');
+        document.querySelector('.bodymove_tabs_tabsInner').style.left = ((100 * active) * -1 ) + '%';
     };
 
 
